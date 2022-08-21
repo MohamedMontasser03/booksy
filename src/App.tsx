7eccd76b237d320c2bb0e47e2c724dd2ee37fb9e
Mohamed Montasser <mohamed.montasser0385@gmail.com>
2022-08-21 13:53:47 +0200

    finished header styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Search } from "tabler-icons-react";
+import { Search, Heart } from "tabler-icons-react";
 
 function App() {
   return (
     <div className="container mx-auto">
-      <header className="flex items-center justify-between flex-wrap p-4">
+      <header className="flex items-center justify-between flex-wrap p-6">
         <h1 className="App text-xl font-semibold">Booksy</h1>
         <div className="w-1/2 relative">
           <input
@@ -11,11 +11,26 @@ function App() {
             type="text"
             placeholder="Search by author, title, or name"
           />
-          <button className="absolute right-4 top-1/2 -translate-y-1/2 flex items-center justify-center rounded-full w-8 h-8 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:shadow-outline">
+          <button className="absolute right-4 top-1/2 -translate-y-1/2 flex items-center justify-center rounded-full w-8 h-8 bg-gray-100 focus:outline-none focus:shadow-outline">
             <Search size={20} />
           </button>
         </div>
-        <div className="flex items-center justify-between"></div>
+        <div className="flex items-center gap-5">
+          <a className="flex items-center hover:bg-gray-200 transition-colors justify-center rounded-full w-8 h-8 focus:outline-none focus:shadow-outline">
+            <Heart size={20} />
+          </a>
+          <button className="flex items-center hover:bg-gray-200 transition-colors justify-center rounded-full w-8 h-8 focus:outline-none focus:shadow-outline">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/1053/1053244.png?w=360"
+              width={40}
+              height={40}
+              alt="user"
+            />
+          </button>
+          <button className="flex items-center hover:bg-gray-200 transition-colors justify-center rounded-full w-8 h-8 focus:outline-none focus:shadow-outline">
+            <span className="font-semibold text-gray-500 opacity-80">EN</span>
+          </button>
+        </div>
       </header>
     </div>
   );
